4d77f13e1584dae979196dc8ff707778176d1f97
romir kulshrestha <romir.kulshrestha@gmail.com>
2024-01-27 16:06:40 +0100

    don't blink

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, thread, time::Duration};
+
+use rppal::{gpio::Gpio, system::DeviceInfo};
+
+const GPIO_LED: u8 = 16;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
+
+    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
+
+    // Blink the LED by setting the pin's logic level high for 500 ms.
+    while (true) {
+        pin.set_high();
+        thread::sleep(Duration::from_millis(500));
+        pin.set_low();
+        thread::sleep(Duration::from_millis(500));
+    }
+
+    Ok(())
 }
